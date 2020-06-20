835d101c40bf8204a7f453e96089c77f317098f8
guillaumekugener <guillaumekugener93@gmail.com>
2020-06-20 14:20:39 -0700

    Updates to annotation cropping and rescale
    - Now works directly from the zip file downloaded from github after CVAT upload
    - Handles all of the renaming and rescaling and puts the outputs into a directory
    - Is easily able to handle multiple zips pointing to the same video
    - Currently does not have any internal checks if there are objects that are not annotated in a certain frame

--- file-preprocessing/annotation-crop-rescale.py
+++ file-preprocessing/annotation-crop-rescale.py
@@ -8,16 +8,21 @@ This script should be applied on a directory that has xml files that are TF read
 import sys
 import os
 import time
+import re
 import tqdm
 
 from absl import app, flags, logging
 from absl.flags import FLAGS
 
 from math import ceil, floor
-
 from lxml import etree
 
-flags.DEFINE_string('source', '', 'path to annotation directory')
+sys.path.insert(1, '/Users/guillaumekugener/Documents/USC/USC_docs/ml/surgical-training-project/tools')
+from surgical_video_annotation import SurgicalVideoAnnotation
+
+# This zip contains the dump file
+flags.DEFINE_string('source', '', 'zip file with the annotations')
+flags.DEFINE_string('vid', '', 'video trial ID')
 flags.DEFINE_string('output', '','path to output directory')
 flags.DEFINE_string('original_size', '1280x720', 'size of original image (pass as NNNxNNN)')
 flags.DEFINE_integer('size', 200, 'resize images to')
@@ -25,16 +30,23 @@ flags.DEFINE_integer('buffer', 10, 'adds a buffer to the cropping')
 
 FLAGS(sys.argv)
 
-ANNOTATION_DIR = FLAGS.source
 OUTPUT_DIR = FLAGS.output
 IMG_SIZE = FLAGS.size
 image_shape = [int(i) for i in FLAGS.original_size.split('x')]
 
-all_frames = [a for a in os.listdir(ANNOTATION_DIR)]
+# Make our SurgicalVideoAnnotation object
+sva = SurgicalVideoAnnotation(FLAGS.source, trial_id=FLAGS.vid)
+
+# Makes the xml from the CVAT output
+for i in sva.frames:
+    i.generate_frame_annotation_xml(OUTPUT_DIR, FLAGS.vid)
+
+# All our frames are in here (and appropriately named)
+all_frames = [a for a in os.listdir(OUTPUT_DIR) if re.search('\\.xml$', a) and re.search('^' + FLAGS.vid, a)]
 
 for a in tqdm.tqdm(all_frames):
 	# Overlay the fixed annotations on the rescaled image
-	annotation_xml = etree.parse(os.path.join(ANNOTATION_DIR, a))
+	annotation_xml = etree.parse(os.path.join(OUTPUT_DIR, a))
 
 	og_w = 0
 	og_h = 0
@@ -45,8 +57,13 @@ for a in tqdm.tqdm(all_frames):
 		for sn in node.iter('height'):
 			og_h = float(sn.text)
 			sn.text = str(IMG_SIZE) # Have to change the height and width
+		
+		# If it already has been rescalled to this size, then don't apply these transformations
+		if og_w == IMG_SIZE and og_h == IMG_SIZE:
+			# No changes have to be made
+			continue
 
-		left_crop = image_shape[0]/2-1 - image_shape[1]/2 - FLAGS.buffer
+		left_crop = og_w/2-1 - og_h/2 - FLAGS.buffer
 		for corner in ['xmin', 'xmax', 'ymin', 'ymax']:    
 			for xml_corner in annotation_xml.iter(corner):
 				if corner == 'xmin' or corner == 'xmax':
