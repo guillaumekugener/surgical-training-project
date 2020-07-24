6c4f7f471edee975eca1e222ec917331a0193b44
guillaumekugener <guillaumekugener93@gmail.com>
2020-07-23 19:43:52 -0700

    Added plotting functions
    - Can plot individual points of bbox positions
    - Can plot the path of a tool's center across video
    - Plot a heatmap of a tool's bounding box across a trial

--- feature_engineering/ica_trial_features.py
+++ feature_engineering/ica_trial_features.py
@@ -3,7 +3,10 @@ import numpy as np
 import pandas as pd
 import re
 
-from math import sqrt
+import matplotlib.pyplot as plt
+from seaborn import scatterplot
+
+from math import sqrt, floor, ceil
 
 """
 ICATrialFeatures
@@ -15,7 +18,7 @@ class ICATrialFeatures:
 	def __init__(
 		self,
 		csv_file_name,
-		csv_class_file
+		csv_class_file,
 	):
 		self.csv_file_name = csv_file_name
 		
@@ -38,9 +41,19 @@ class ICATrialFeatures:
 		videos = []
 
 		for vid in vids:
-			videos.append(VideoTrial(video_data=self.data[self.data['vid']==vid]))
+			videos.append(VideoTrial(video_data=self.data[self.data['vid']==vid], video_id=vid))
 
 		return videos
+
+	"""Get video by ID"""
+	def get_video_by_id(self, vid):
+		video = None
+		for i in self.videos:
+			if i.get_video_id() == vid:
+				video = i
+				break
+
+		return video
 """
 VideoTrial
 
@@ -49,10 +62,39 @@ Describes an individual video trial
 class VideoTrial:
 	def __init__(
 		self,
-		video_data
+		video_data,
+		video_id, 
+		frame_size=None
 	):
 		self.video_data = video_data.reset_index()
+		self.video_id = video_id
+		self.frame_size = self.__set_frame_size(frame_size)
 		self.frames = self.__build_frames()
+
+	"""
+	Set video size
+
+	If no video size is passed in, we are going to guess 
+	what the size should be based on the bounding box coordinates
+	"""
+	def __set_frame_size(self, frame_size):
+		if frame_size is None:
+			"""Make a guess"""
+			max_x = int(max(self.video_data['x2']))
+			max_y = int(max(self.video_data['y2']))
+
+			if max_y == 720:
+				frame_size = (1280, 720)
+			elif max_y == 1280:
+				frame_size = (1920, 1080)
+			else:
+				pass
+
+		return frame_size
+
+	"""Get trial id"""
+	def get_video_id(self):
+		return self.video_id
 
 	"""Get length of video"""
 	def get_video_length(self):
@@ -64,7 +106,10 @@ class VideoTrial:
 		unique_frames = set([i for i in self.video_data['file']])
 
 		for f in unique_frames:
-			frames.append(IndividualFrame(self.video_data[self.video_data['file']==f]))
+			frames.append(IndividualFrame(
+				frame_size=self.frame_size,
+				frame_data=self.video_data[self.video_data['file']==f]
+			))
 
 		# We need them to be sorted for future operations
 		frames.sort(key=lambda f: f.frame_name)
@@ -158,6 +203,89 @@ class VideoTrial:
 
 		return velocities
 
+	"""
+	Plot tools over all frames
+
+	Plots the center of each tool across all frames (as a scatter plot)
+	"""
+	def plot_tool_positions(
+		self, 
+		tools=[],
+		n_frames=None
+	):
+		# We have to get the plot data
+		positions_dict = { 'x': [], 'y': [], 'class': []}
+		for tool in tools:
+			positions = self.get_tool_path(tool)
+
+			# If we want to only plot data for the first n frames
+			if n_frames is not None:
+				positions = positions[:n_frames]
+
+			positions_dict['x'] += [i[0] for i in positions]
+			positions_dict['y'] += [i[1] for i in positions]
+			positions_dict['class'] += [tool for i in positions]
+
+		positions_df = pd.DataFrame(positions_dict)
+
+		ax = scatterplot(data=positions_df, x='x', y='y', hue='class')
+		ax.invert_yaxis()
+
+	"""
+	Plot paths across frames
+
+	Creates a plot connecting the dots across frames for the different tools
+	"""
+	def plot_tool_paths(
+		self, 
+		tools=[],
+		tool_colors={
+			'suction': 'blue',
+			'grasper': 'orange',
+			'cottonoid': 'green',
+			'muscle': 'red',
+			'string': 'purple'
+		}
+	):
+		positions_dict = { 
+			'x': [], 'y': [],
+			'xend': [], 'yend': [],
+			'class': [] 
+		}
+
+		for tool in tools:
+			positions = self.get_tool_path(tool)
+			positions_dict['x'] += [i[0] for i in positions[:len(positions)-1]]
+			positions_dict['y'] += [i[1] for i in positions[:len(positions)-1]]
+
+			positions_dict['xend'] += [i[0] for i in positions[1:]]
+			positions_dict['yend'] += [i[1] for i in positions[1:]]
+
+			positions_dict['class'] += [tool for i in positions[1:]]
+
+		positions_df = pd.DataFrame(positions_dict)
+
+		for r in range(positions_df.shape[0]):
+			x, y, xend, yend, cl = positions_df.loc[r, ['x', 'y', 'xend', 'yend', 'class']]
+
+			if x is None:
+				continue
+
+			plt.plot([x, xend], [y, yend], c=tool_colors[cl], ls='-')
+
+		plt.show()
+
+	"""
+	Plot tool positions as heatmap
+	"""
+	def plot_tool_heatmap(self, tool):
+		tool_heatmap = np.zeros((self.frame_size[1], self.frame_size[0]))
+		for f in self.frames:
+			tool_heatmap += f.get_tool_bbox_binary(tool)
+
+		plt.imshow(tool_heatmap, cmap='hot', interpolation='nearest')
+		plt.show()
+
 """
 IndividualFrame
 
@@ -166,11 +294,13 @@ Describes an individual frame
 class IndividualFrame:
 	def __init__(
 		self,
+		frame_size,
 		frame_data
 	):
 		self.frame_data = frame_data.reset_index()
 		self.frame_name = self.frame_data['file'][0]
 		self.tools = self.get_tools_in_frame()
+		self.frame_size = frame_size
 
 	"""Get the tools in this frame"""
 	def get_tools_in_frame(self):
@@ -198,6 +328,37 @@ class IndividualFrame:
 
 		return position
 
+	"""Get particular tool"""
+	def get_specific_tool(self, tool):
+		relevant_tools = []
+		for to in self.tools:
+			if to.get_type() == tool:
+				relevant_tools.append(to)
+
+		return relevant_tools
+
+	"""
+	Tool bbox binary
+	
+	Returns a numpy array the size of the frame
+	with 1s where the bounding box is and 0s everywhere
+	else
+	"""
+	def get_tool_bbox_binary(self, tool):
+		frame_array = np.zeros((self.frame_size[1], self.frame_size[0]))
+
+		relevant_tools = self.get_specific_tool(tool)
+
+		for to in relevant_tools:
+			x1, y1, x2, y2 = [int(i) for i in to.get_standard_coordinates()]
+
+
+			tool_array = np.ones((y2-y1, x2-x1))
+			frame_array[y1:y2,x1:x2] += tool_array
+
+		return frame_array
+
+
 """
 SurgicalTool
 
@@ -221,6 +382,10 @@ class SurgicalTool:
 	def get_tool_position(self):
 		return self.get_bbox_center()
 
+	"""Get original x1, y1, x2, y2"""
+	def get_standard_coordinates(self):
+		return self.bbox_coordinates
+
 	"""Get the center of the bbox"""
 	def get_bbox_center(self):
 		xc = self.bbox_coordinates[2] - self.bbox_coordinates[0]
@@ -228,9 +393,41 @@ class SurgicalTool:
 
 		return (xc, yc)
 
+	"""Get tool corners"""
+	def get_tool_corners(self):
+		tl = self.bbox_coordinates[0], self.bbox_coordinates[1]
+		bl = self.bbox_coordinates[0], self.bbox_coordinates[3]
+		tr = self.bbox_coordinates[2], self.bbox_coordinates[1]
+		br = self.bbox_coordinates[2], self.bbox_coordinates[3]
+
+		return tl, bl, tr, br
+
+	"""Get bbox dimensions"""
+	def get_dimensions(self):
+		h = self.bbox_coordinates[3] - self.bbox_coordinates[1]
+		w = self.bbox_coordinates[2] - self.bbox_coordinates[0]
+		return w, h
+
+	"""Get tool area"""
+	def get_area(self):
+		w, h = self.get_dimensions()
+		return w * h
 
 
+"""
+Calculate overlapping area of two boxes
 
+Assumes that the coordinates for the boxes
+are x1, y1, x2, y2
+
+Returns None if there is no overlap
+"""
+def overlap_area(a, b):
+	dx = min(a[2], b[2]) - max(a[0], b[0])
+	dy = min(a[3], b[3]) - max(a[1], b[1])
+
+	if (dx >= 0) and (dy >= 0):
+		return dx * dy
 
 
 
